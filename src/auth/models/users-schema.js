b1669badf3dd7019e0631e3ae0676b4eccfc76bb
Amer-bit <ny.1995@outlook.com>
2020-06-08 08:58:10 +0300

    notReadyYet

--- src/auth/models/users-schema.js
+++ src/auth/models/users-schema.js
@@ -1,16 +1,21 @@
 'use strict';
 
 const mongoose  = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
   username: {type: String, required: true},
   password: {type: String, required :true},
 });
 
-// userSchema.methods.auth = function (hashed){
+userSchema.methods.auth =  function (username, password){
 // if(hashed){read if read return then compare wiht the hashed 
-
-// }
+  this.find({username}).then(data => {
+    bcrypt.compare(password, data[0].password).then(result => {
+      return  result ? data : 'invalid password';
+    });
+  });
+};
 // };
 
 module.exports = mongoose.model('userModel', userSchema);
